968f98befc73aef60589e7f51bc25b676e5478cc
DreamingCuttlefish <mossyforest@posteo.net>
2024-05-31 00:07:01 -0400

    try adding repo manually instead

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -4,7 +4,11 @@ set -ouex pipefail
 
 # Download mullvad rpm
 # curl -o mullvad-client.rpm -OJL https://mullvad.net/download/app/rpm/latest
-wget -P /tmp/mullvad-client.rpm https://mullvad.net/download/app/rpm/latest
+# wget -P /tmp/mullvad-client.rpm https://mullvad.net/download/app/rpm/latest
+mkdir /etc
+mkdir /etc/yum.repos.d
+curl -o /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
 
-rpm-ostree install /tmp/mullvad-client.rpm
+# rpm-ostree install /tmp/mullvad-client.rpm
+rpm-ostree install mullvad-vpn
 
