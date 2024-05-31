826e17b1ae47e11dff9aa69bd8b8ea6ca6648d47
DreamingCuttlefish <mossyforest@posteo.net>
2024-05-31 00:01:27 -0400

    change mullvad installation method from curl to wget

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,7 +3,8 @@
 set -ouex pipefail
 
 # Download mullvad rpm
-curl -o mullvad-client.rpm -OJL https://mullvad.net/download/app/rpm/latest
+# curl -o mullvad-client.rpm -OJL https://mullvad.net/download/app/rpm/latest
+wget -P /tmp/mullvad-client.rpm https://mullvad.net/download/app/rpm/latest
 
 rpm-ostree install /tmp/mullvad-client.rpm
 
