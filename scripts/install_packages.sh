3856c15387b60fc6f879a9123daf55b83fee2fda
DreamingCuttlefish <mossyforest@posteo.net>
2024-05-31 00:13:43 -0400

    remove second unnecessary mkdir in install_packages.sh

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -5,7 +5,6 @@ set -ouex pipefail
 # Download mullvad rpm
 # curl -o mullvad-client.rpm -OJL https://mullvad.net/download/app/rpm/latest
 # wget -P /tmp/mullvad-client.rpm https://mullvad.net/download/app/rpm/latest
-mkdir /etc/yum.repos.d
 curl -o /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
 
 # rpm-ostree install /tmp/mullvad-client.rpm
