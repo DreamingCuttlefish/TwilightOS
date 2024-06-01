1c927b48afdd3f911fbec544fefdcb0b57426fe0
DreamingCuttlefish <mossyforest@posteo.net>
2024-06-01 12:22:35 -0400

    add supergfxctl plasmoid

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -6,8 +6,10 @@ echo "Creating directories..."
 mkdir -p "/opt/Mullvad VPN/resources"
 mkdir -p "/var/log/mullvad-vpn"
 
-echo "Downloading Mullvad repository file..."
+echo "Downloading repository files..."
 curl -o /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
+curl -o /etc/yum.repos.d/jhyub-supergfxctl-plasmoid-fedora-40.repo https://copr.fedorainfracloud.org/coprs/jhyub/supergfxctl-plasmoid/repo/fedora-40/jhyub-supergfxctl-plasmoid-fedora-40.repo
 
 echo "Installing Mullvad VPN package..."
 rpm-ostree install mullvad-vpn
+rpm-ostree install supergfxctl-plasmoid
