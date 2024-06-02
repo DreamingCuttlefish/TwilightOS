c3a4022f04b23aa41cb76ae0c8e58b4589537720
DreamingCuttlefish <mossyforest@posteo.net>
2024-06-02 01:09:29 -0400

    try again to fix

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -3,8 +3,8 @@
 set -ouex pipefail
 
 echo "Creating directories..."
-#mkdir -p "/opt/Mullvad VPN/resources"
-#mkdir -p "/var/log/mullvad-vpn"
+mkdir -p "/opt/Mullvad VPN/resources"
+mkdir -p "/var/log/mullvad-vpn"
 
 echo "Downloading repository files..."
 curl -Lo /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
@@ -12,4 +12,7 @@ curl -Lo /etc/yum.repos.d/jhyub-supergfxctl-plasmoid-fedora-40.repo https://copr
 
 echo "Installing Mullvad VPN package..."
 rpm-ostree install mullvad-vpn
+
+echo "Installing supergfxctl + it's plasmoid..."
+rpm-ostree install supergfxctl
 rpm-ostree install supergfxctl-plasmoid
--- scripts/preparation.sh
+++ scripts/preparation.sh
@@ -3,6 +3,7 @@
 echo "Setting up symlinks for /opt"
 
 mkdir -p "/var/opt"
+mkdir -p "/usr/lib/opt"
 
 ln -s "/var/opt" "/opt"
 
