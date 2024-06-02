ff90df1696350b9ac831ae210dbd384b38a9e072
DreamingCuttlefish <mossyforest@posteo.net>
2024-06-02 00:19:20 -0400

    try to fix packages installation

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -7,8 +7,8 @@ mkdir -p "/opt/Mullvad VPN/resources"
 mkdir -p "/var/log/mullvad-vpn"
 
 echo "Downloading repository files..."
-curl -o /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
-curl -o /etc/yum.repos.d/jhyub-supergfxctl-plasmoid-fedora-40.repo https://copr.fedorainfracloud.org/coprs/jhyub/supergfxctl-plasmoid/repo/fedora-40/jhyub-supergfxctl-plasmoid-fedora-40.repo
+curl -Lo /etc/yum.repos.d/mullvad.repo https://repository.mullvad.net/rpm/stable/mullvad.repo
+curl -Lo /etc/yum.repos.d/jhyub-supergfxctl-plasmoid-fedora-40.repo https://copr.fedorainfracloud.org/coprs/jhyub/supergfxctl-plasmoid/repo/fedora-40/jhyub-supergfxctl-plasmoid-fedora-40.repo
 
 echo "Installing Mullvad VPN package..."
 rpm-ostree install mullvad-vpn
